b7e8ad34163781003290847bc0373caafb448ec6
ottolahde <otto.lahde@gmail.com>
2021-09-24 18:35:55 +0300

    initial

--- homework/2021-38/e03-e04.js/index.js
+++ homework/2021-38/e03-e04.js/index.js
@@ -1,5 +1,6 @@
-var readlineSync = require('readline-sync');
-
+const readlineSync = require('readline-sync');
+const fs = require('fs')
 
 var filename = readlineSync.question('Give file name: ');
 console.log('The content of the file is: ')
+console.log(fs.readFileSync(filename,'utf-8'));
